f590bb50cdeca4a3b24c37874fc5f3f7c21286ee
nhoga <n9746ab@gmail.com>
2021-03-09 20:49:48 -0300

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,51 @@
+import data from './data'
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header className="row">
+        <div>
+          <a className="brand" href="/">nhogazon</a>
+        </div>
+        <div>
+          <a href="/cart">Carrito de compras</a>
+          <a href="/signin">Iniciar sesion</a>
+        </div>
+      </header>
+      <main>
+        <div>
+          <div className="row center">
+            {
+              data.products.map(product => (
+                <div key={product._id} className="card">
+                  <a href={`/product/${product._id}`}>
+
+                    <img className="medium" src={product.image} alt={product.name} />
+                  </a>
+                  <div className="card-body">
+                    <a href={`/product/${product._id}`}>
+                      <h2>{product.name}</h2>
+                    </a>
+                    <div className="rating">
+                      <span> <i className="fa fa-star"></i> </span>
+                      <span> <i className="fa fa-star"></i> </span>
+                      <span> <i className="fa fa-star"></i> </span>
+                      <span> <i className="fa fa-star"></i> </span>
+                      <span> <i className="fa fa-star-half-o"></i> </span>
+                    </div>
+                    <div className="price">${product.price}</div>
+                  </div>
+                </div>
+              ))
+            }
+
+          </div>
+        </div>
+      </main>
+      <footer className="row center">Todos los derechos reservados @ nhoga</footer>
+    </div>
+
+  )
+}
+
+export default App;
---
+++ frontend/src/data.js
@@ -0,0 +1,73 @@
+const data = {
+    products: [
+        {
+            _id: 1,
+            name: 'LaCamisuli',
+            category: 'Shirts',
+            image: '/images/p3.jpg',
+            price: 12400,
+            brand: 'Nike',
+            rating: 4.9,
+            numReviews: 10,
+            description: 'producto de excelente calidad, peruana'
+        },
+        {
+            _id: 2,
+            name: 'Pantalones Lacoste',
+            category: 'Pantalones',
+            image: '/images/p4.jpg',
+            price: 22400,
+            brand: 'Lacoste',
+            rating: 4.3,
+            numReviews: 10,
+            description: 'producto de excelente calidad, peruana'
+        },
+        {
+            _id: 3,
+            name: 'Queseya',
+            category: 'Queseya',
+            image: '/images/p2.jpg',
+            price: 321,
+            brand: 'Puma',
+            rating: 4.9,
+            numReviews: 10,
+            description: 'producto de excelente calidad, peruana'
+        },
+        {
+            _id: 4,
+            name: 'LaCamisuli',
+            category: 'Shirts',
+            image: '/images/p3.jpg',
+            price: 8931981,
+            brand: 'Nike',
+            rating: 4.9,
+            numReviews: 10,
+            description: 'producto de excelente calidad, peruana'
+        },
+        {
+            _id: 5,
+            name: 'LaCamisuli',
+            category: 'Shirts',
+            image: '/images/p3.jpg',
+            price: 1,
+            brand: 'Nike',
+            rating: 4.9,
+            numReviews: 10,
+            description: 'producto de excelente calidad, peruana'
+        },
+        {
+            _id: 6,
+            name: 'LaCamisuli',
+            category: 'Shirts',
+            image: '/images/p3.jpg',
+            price: 35000,
+            brand: 'Nike',
+            rating: 4.9,
+            numReviews: 10,
+            description: 'producto de excelente calidad, peruana'
+        },
+
+    ],
+};
+
+export default data;
