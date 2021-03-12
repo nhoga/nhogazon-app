70220ab3a07946fb8eaa859892943ce33ab0f4b6
nhoga <n9746ab@gmail.com>
2021-03-12 04:14:30 -0300

    conexion lograda, homescreen & backend

---
+++ backend/data.js
@@ -0,0 +1,79 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'asdasdsa',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 12400,
+            countInStock: 12,
+            brand: 'Nike',
+            rating: 1.3,
+            numReviews: 2,
+            description: 'producto de excelente calidad, peruana'
+        },
+        {
+            _id: '2',
+            name: 'Remera Lacoste',
+            category: 'Shirts',
+            image: '/images/p2.jpg',
+            price: 22400,
+            countInStock: 1,
+            brand: 'Lacoste',
+            rating: 4.3,
+            numReviews: 31,
+            description: 'producto de excelente calidad, peruana'
+        },
+        {
+            _id: '3',
+            name: 'Queseya',
+            category: 'Queseya',
+            countInStock: 0,
+            image: '/images/p3.jpg',
+            price: 321,
+            brand: 'Puma',
+            rating: 4.9,
+            numReviews: 41,
+            description: 'producto de excelente calidad, peruana'
+        },
+        {
+            _id: '4',
+            name: 'Pantalones LaPosta',
+            category: 'Pantalones',
+            image: '/images/p4.jpg',
+            price: 8931981,
+            countInStock: 3,
+            brand: 'Adidas',
+            rating: 2.9,
+            numReviews: 1,
+            description: 'producto de excelente calidad, peruana'
+        },
+        {
+            _id: '5',
+            name: 'Pantalones Rey',
+            category: 'Pantalones',
+            image: '/images/p5.jpg',
+            countInStock: 21,
+            price: 1,
+            brand: 'NewBalance',
+            rating: 0,
+            numReviews: 421,
+            description: 'producto de excelente calidad, peruana'
+        },
+        {
+            _id: '6',
+            name: 'Zapatos LaClandestina',
+            category: 'Zapatos',
+            image: '/images/p6.jpg',
+            price: 35000,
+            brand: 'Nike',
+            countInStock: 0,
+            rating: 3.1,
+            numReviews: 0,
+            description: 'producto de excelente calidad, peruana'
+        },
+
+    ],
+};
+
+export default data;
